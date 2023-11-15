d02b4a5d793584a12f3c0be6dae6e2b86e801a6a
Brisa Marca <brisa.marca@davinci.edu.ar>
2023-11-15 16:10:59 -0300

    estilos tabla-planes

--- proyect-concepto/src/pages/Mensajes.vue
+++ proyect-concepto/src/pages/Mensajes.vue
@@ -16,7 +16,8 @@ export default{
     <div class="content-login" >
         <h1 class="hero-section-title mb-24">Administrar mensajes de clientes</h1>
     </div>
-    <div class="content-users-admin">
+    <!--Realizar un v-for en dodne se pueda ver los mensajes de la colección-->
+    <div class="content-users-admin" >
         <div class="card-users">
             <img src="../../assets/svg/users-icons.svg" alt="" width="80px" height="80px">
             <h3>brisa@gmail.com</h3>
