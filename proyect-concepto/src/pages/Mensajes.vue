9f1cb2308d47eaed6d0aa7bda188738dde0a9f79
Brisa-Marca <brisa.marca@davinci.edu.ar>
2023-12-05 23:39:13 -0300

    lista de usuarios por parte del usuario administrador

--- proyect-concepto/src/pages/Mensajes.vue
+++ proyect-concepto/src/pages/Mensajes.vue
@@ -36,22 +36,5 @@ export default {
             <button  type="submit" class="btn-users"> Ver mensajes</button>
 
         </div>
-        <!-- <div class="card-users">
-            <img src="../../assets/svg/users-icons.svg" alt="" width="80px" height="80px">
-            <h3>brisa@gmail.com</h3>
-            <button class="btn-users" > Ver mensajes</button>
-          
-        </div>
-        <div class="card-users">
-            <img src="../../assets/svg/users-icons.svg" alt="" width="80px" height="80px">
-            <h3>ana@gmail.com</h3>
-            <button class="btn-users">Ver mensajes</button>
-        </div>
-        <div class="card-users">
-            <img src="../../assets/svg/users-icons.svg" alt="" width="80px" height="80px">
-            <h3>juan@gmail.com</h3>
-            <button class="btn-users">Ver mensajes</button>
-        </div> -->
-
     </div>
 </template>
