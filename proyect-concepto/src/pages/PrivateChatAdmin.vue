758f9c5d43abf9248bf81d02f98579ee1bc7baa9
Brisa-Marca <brisa.marca@davinci.edu.ar>
2023-12-07 19:42:37 -0300

    chat privado por parte del usuario administrador con un usuario autenticado

--- proyect-concepto/src/pages/PrivateChatAdmin.vue
+++ proyect-concepto/src/pages/PrivateChatAdmin.vue
@@ -1,18 +1,56 @@
 <script>
 import { getUserProfileById } from '../services/user';
+import { sendPrivateChatMessage, subscribeToPrivateChat } from '../services/private-chat';
+import { subscribeToAuth } from '../services/auth';
+import { formatDate } from '../helpers/date.js';
+
 export default {
     name: 'PrivateChatAdmin',
     data() {
         return {
+            messages: [],
             userProfile: {
                 id: null,
                 email: null,
-            }
+            },
+            newMessage: {
+                message: '',
+            },
+            userAuth: {
+                id: null,
+                email: null,
+
+            },
+            authUnsubscribe: () => { },
+            chatUnsubscribe: () =>{},
         }
+    },
+    methods: {
+        handleMessage() {
+            sendPrivateChatMessage({
+                sender: this.userAuth.id,
+                receiver: this.userProfile.id,
+                message: this.newMessage.message,
+            });
+             this.newMessage.message = '';
+        },
+        dateToString(date) {
+            return formatDate(date);
+        }
+
     },
     async mounted() {
         this.userProfile = await getUserProfileById(this.$route.params.id);
+        this.authUnsubscribe = subscribeToAuth(user => this.userAuth = user);
+        this.chatUnsubscribe = await subscribeToPrivateChat({
+            user1:this.userProfile.id,
+            user2:this.userAuth.id, 
 
+        }, newMessages => this.messages = newMessages);
+    
+    },
+    unmounted() {
+        this.authUnsubscribe();
     }
 }
 </script>
@@ -23,15 +61,20 @@ export default {
 
 
     <section class="content-chat">
-        <div id="chat" class="chat-admin">
-            <p>hola</p>
-            <div class="fecha-hora ">111</div>
+        <div class="content-chat-users">
+            <div id="chat" class="chat-admin" v-for="message in messages">
+            <p>{{ message.message }}</p>
+            <div class="fecha-hora ">{{ dateToString(message.created_at) || 'Enviando...' }}</div>
         </div>
+
+        </div>
+        
         <div class="form-chat " id="chat-form">
-            <form action="#" class="content-form-chat">
+            <form action="#" class="content-form-chat" @submit.prevent="handleMessage">
                 <div class="content-textarea">
                     <label for="mensaje"></label>
-                    <textarea class="textarea" type="text" id="mensaje" placeholder="Hola,tengo una duda..."></textarea>
+                    <textarea class="textarea" type="text" id="mensaje" placeholder="hola,admin..."
+                        v-model="newMessage.message"></textarea>
                 </div>
                 <div>
                     <button type="submit" class="main-cta enviar">Enviar</button>
